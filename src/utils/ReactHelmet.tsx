41346f87604759b246e0d8b2025c122f2f132079
minntoko <minntoko0822@gmail.com>
2024-03-11 00:46:07 +0900

    add: OGPを追加

---
+++ src/utils/ReactHelmet.tsx
@@ -0,0 +1,28 @@
+import { Helmet } from "react-helmet";
+
+const ReactHelmet = () => {
+  const url = window.location.href;
+  return (
+    <Helmet>
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="ポートフォリオ" />
+      <meta
+        property="og:description"
+        content="今まで作ってきた作品やスキルを紹介するポートフォリオサイトです。"
+      />
+      <meta property="og:site_name" content="知也のポートフォリオサイト" />
+      <meta property="og:image" content={`${url}img/portfolioOGP.png`} />
+      <meta property="og:locale" content="ja_JP" />
+
+      <meta name="keywords" content="ポートフォリオ,フロントエンド,学生,React" />
+      <meta
+        content="HAL名古屋のフロントエンドエンジニアを目指している学生です。
+        今まで作ってきた作品やプログラミングスキルを紹介するポートフォリオサイトです。"
+        name="description"
+      ></meta>
+    </Helmet>
+  );
+};
+
+export default ReactHelmet;
